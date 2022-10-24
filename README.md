183589e2a38d3f4d6ffa6290ce768a1bb4f44fcc
GUETIN Arnaud <guetin.arnaud@gmail.com>
2022-10-24 19:32:30 +0200

    ✨ Init AWS formation project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# AWSProject
+ # AWSProject
 FormationAWS
---
+++ src/main/java/com/yirmea/aws/AwsApplication.java
@@ -0,0 +1,16 @@
+package com.yirmea.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class AwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsApplication.class, args);
+	}
+
+}
