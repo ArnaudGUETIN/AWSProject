e2b25d33883c9dff01f23c6f12e67fde815cfea0
GUETIN Arnaud <guetin.arnaud@gmail.com>
2022-10-24 20:39:52 +0200

    ✨ expose API for Amazon s3 ops

--- src/main/java/com/yirmea/aws/AwsApplication.java
+++ src/main/java/com/yirmea/aws/AwsApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableSwagger2
 public class AwsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/yirmea/aws/web/BucketController.java
@@ -0,0 +1,68 @@
+package com.yirmea.aws.web;
+
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.yirmea.aws.service.AmazonS3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/buckets/")
+@RequiredArgsConstructor
+public class BucketController {
+
+    private final AmazonS3Service s3Service;
+
+    @PostMapping(value = "/{bucketName}")
+    public void createBucket(@PathVariable String bucketName, @RequestParam boolean publicBucket){
+        s3Service.createS3Bucket(bucketName, publicBucket);
+    }
+
+    @GetMapping
+    public List<String> listBuckets(){
+        var buckets = s3Service.listBuckets();
+        var names = buckets.stream().map(Bucket::getName).collect(Collectors.toList());
+        return names;
+    }
+
+    @DeleteMapping(value = "/{bucketName}")
+    public void deleteBucket(@PathVariable String bucketName){
+        s3Service.deleteBucket(bucketName);
+    }
+
+//    @PostMapping(value = "/{bucketName}/objects")
+//    public void createObject(@PathVariable String bucketName, @RequestBody BucketObjectRepresentaion representaion, @RequestParam boolean publicObject) throws IOException {
+//        s3Service.putObject(bucketName, representaion, publicObject);
+//    }
+
+    @GetMapping(value = "/{bucketName}/objects/{objectName}")
+    public File downloadObject(@PathVariable String bucketName, @PathVariable String objectName) throws IOException {
+        s3Service.downloadObject(bucketName, objectName);
+        return new File("./" + objectName);
+    }
+
+    @PatchMapping(value = "/{bucketName}/objects/{objectName}/{bucketSource}")
+    public void moveObject(@PathVariable String bucketName, @PathVariable String objectName, @PathVariable String bucketSource) throws IOException {
+        s3Service.moveObject(bucketName, objectName, bucketSource);
+    }
+
+    @GetMapping(value = "/{bucketName}/objects")
+    public List<String> listObjects(@PathVariable String bucketName) throws IOException {
+        return s3Service.listObjects(bucketName).stream().map(S3ObjectSummary::getKey).collect(Collectors.toList());
+    }
+
+    @DeleteMapping(value = "/{bucketName}/objects/{objectName}")
+    public void deleteObject(@PathVariable String bucketName, @PathVariable String objectName) throws IOException {
+        s3Service.deleteObject(bucketName, objectName);
+    }
+
+    @DeleteMapping(value = "/{bucketName}/objects")
+    public void deleteObject(@PathVariable String bucketName, @RequestBody List<String> objects) throws IOException {
+        s3Service.deleteMultipleObjects(bucketName, objects);
+    }
+}
